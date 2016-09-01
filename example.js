8cd7cd68eca05ce50900fbff321b5c6bba4ad983
Luke Burns <lukemburns@gmail.com>
2016-09-01 16:21:46 -0400

    fix example

--- example.js
+++ example.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
 var PeerEmitter = require('.')
 
-var peer = new PeerEmitter()
-console.log('Your name is:', window.peer.keyPair.publicKey.toString('hex').slice(0, 5))
+var emitter = new PeerEmitter()
+console.log('Your name is:', emitter.keyPair.publicKey.toString('hex').slice(0, 5))
 console.log('To open the greetings channel, type \'greetings\'')
-console.log('To send \'hello world\' to peers on \'greetings\', type \'greetings : hello world\'')
+console.log('To send \'hello world\' to emitters on \'greetings\', type \'greetings : hello world\'')
 
 process.stdin.on('data', function (data) {
   var split = data.toString().split(':'),
       channel = split[0].trim(),
       msg = (split[1] || '').trim()
 
-  peer.emit(channel, msg)
+  emitter.emit(channel, msg)
 
-  if (!peer.client._events[channel]) {
-    peer.on(channel, function (message) {
+  if (!emitter.client._events[channel]) {
+    emitter.on(channel, function (message) {
       console.log(channel, ':', message.toString())
     })
   }
 })
 
-peer.client.on('connection', function (name, handshake) {
+emitter.client.on('connection', function (name, handshake) {
   var id = handshake.id.toString('hex').slice(0, 5)
   console.log(id, 'joined channel', name)
 })
